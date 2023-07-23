81e9930a2ed5b2c974757cd30a53836764b8ec71
ylin14 <y.linyvyi@gmail.com>
2023-07-23 14:57:57 +0300

    1

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 import { App } from 'components/App';
 
-import { store, persistor } from 'redux/store.js';
+import { store, persistor } from './redux/store';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
